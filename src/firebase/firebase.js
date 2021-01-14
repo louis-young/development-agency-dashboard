90c7ecd893c7d246d43acc5a768fe9852e78046c
Louis Young <me@louisyoung.co.uk>
2021-01-14 17:32:14 +0000

    Configure `.env`

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const configuration = {
-  apiKey: "AIzaSyADkrFYRSDJZKfYxx4mQ2Rf5iMAJgtfIxY",
-  authDomain: "authentication-de944.firebaseapp.com",
-  databaseURL: "https://authentication-de944-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "authentication-de944",
-  storageBucket: "authentication-de944.appspot.com",
-  messagingSenderId: "1060559987965",
-  appId: "1:1060559987965:web:c792875a729075b6d34a94",
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASE_URL}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
 };
 
 firebase.initializeApp(configuration);

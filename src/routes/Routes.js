6185a7561dc25f94ee26ef7eecc909f2546d42fe
Louis Young <me@louisyoung.co.uk>
2021-01-17 15:32:24 +0000

    Remove `ProtectedRoute` implementation

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,8 +4,6 @@ import { Switch, Route } from "react-router-dom";
 
 import { ROUTES } from "../constants/constants";
 
-import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
-
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Documentation from "../pages/Documentation/Documentation";
 import Domains from "../pages/Domains/Domains";
@@ -14,10 +12,10 @@ import Projects from "../pages/Projects/Projects";
 const Routes = () => {
   return (
     <Switch>
-      <ProtectedRoute path={ROUTES.DASHBOARD} exact component={Dashboard} />
-      <ProtectedRoute path={ROUTES.PROJECTS} component={Projects} />
-      <ProtectedRoute path={ROUTES.DOMAINS} component={Domains} />
-      <ProtectedRoute path={ROUTES.DOCUMENTATION} component={Documentation} />
+      <Route path={ROUTES.DASHBOARD} exact component={Dashboard} />
+      <Route path={ROUTES.PROJECTS} component={Projects} />
+      <Route path={ROUTES.DOMAINS} component={Domains} />
+      <Route path={ROUTES.DOCUMENTATION} component={Documentation} />
     </Switch>
   );
 };

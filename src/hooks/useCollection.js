a5a0c2b6a07f36fd03ad7c53716813fd8774ce1d
Louis Young <me@louisyoung.co.uk>
2021-01-18 19:00:46 +0000

    Spread `id` in to collection data

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -18,7 +18,13 @@ const useCollection = (name) => {
           throw new Error();
         }
 
-        const data = collection.docs.map((document) => document.data());
+        const data = collection.docs.map((document) => {
+          const id = document.id;
+
+          const data = document.data();
+
+          return { id, ...data };
+        });
 
         setData(data);
       } catch (error) {

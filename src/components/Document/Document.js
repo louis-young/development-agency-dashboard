b4da32cce9dc7cbc7c2ec23f5dcade3ab39bbd8d
Louis Young <me@louisyoung.co.uk>
2021-01-19 19:13:25 +0000

    Replace magic strings with constants

--- src/components/Document/Document.js
+++ src/components/Document/Document.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import { ROUTES } from "../../constants/constants";
+
 const Document = ({ id, title }) => {
   return (
     <li>
-      <Link to={`/documentation/document/${id}`}>{title}</Link>
+      <Link to={`${ROUTES.DOCUMENT}${id}`}>{title}</Link>
     </li>
   );
 };
--- src/components/Domain/Domain.js
+++ src/components/Domain/Domain.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import { ROUTES } from "../../constants/constants";
+
 const Domain = ({ id, domain }) => {
   return (
     <li>
-      <Link to={`/domains/domain/${id}`}>{domain}</Link>
+      <Link to={`${ROUTES.DOMAIN}${id}`}>{domain}</Link>
     </li>
   );
 };

8fcee801041ed689defaa91a71d55a3055df701b
Louis Young <me@louisyoung.co.uk>
2021-01-20 22:13:15 +0000

    Add user details to `Sidebar`

--- src/layout/Sidebar/Sidebar.js
+++ src/layout/Sidebar/Sidebar.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { Link } from "react-router-dom";
+
+import { AuthenticationContext } from "../../context/AuthenticationContext";
 
 import { ROUTES } from "../../constants/constants";
 
 const Sidebar = () => {
+  const { user } = useContext(AuthenticationContext);
+
+  const { displayName: name, photoURL: photo } = user;
+
   return (
     <aside>
       <h2>Sidebar</h2>
+
+      <img src={photo} alt={name} />
+      <h3>{name}</h3>
+
       <nav>
         <ul>
           <li>

90e2f3641e7589f0360d81efa907da4c7c1b2470
Louis Young <me@louisyoung.co.uk>
2021-01-20 22:05:31 +0000

    Render domains on `Client` page

--- src/pages/Client/Client.js
+++ src/pages/Client/Client.js
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 
 import { ClientsContext } from "../../context/ClientsContext";
+import { DomainsContext } from "../../context/DomainsContext";
 
 import { ROUTES } from "../../constants/constants";
 
@@ -10,6 +11,8 @@ const Client = () => {
   const { id } = useParams();
 
   const { clients, deleteMutation } = useContext(ClientsContext);
+
+  const { domains } = useContext(DomainsContext);
 
   if (!clients) {
     return <p>Loading client...</p>;
@@ -27,12 +30,24 @@ const Client = () => {
     deleteMutation.mutate(id);
   };
 
+  const clientDomains = domains?.filter((domain) => domain.company === id);
+
   return (
     <section>
       <h1>Client</h1>
 
       <p>Company: {company}</p>
       <p>Contact: {contact}</p>
+
+      <h2>Domains</h2>
+
+      <ul>
+        {clientDomains.map((domain) => (
+          <li>
+            <Link to={`${ROUTES.DOMAINS}/${domain.id}`}>{domain.domain}</Link>{" "}
+          </li>
+        ))}
+      </ul>
 
       <Link to={`${ROUTES.CLIENTS}/${id}/edit`}>Edit</Link>
       <button onClick={deleteClient}>Delete</button>

f91be51f9229e645b71c820236332aa0617672cc
Louis Young <me@louisyoung.co.uk>
2021-01-20 10:23:21 +0000

    Add greeting to `Dashboard`

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { AuthenticationContext } from "../../context/AuthenticationContext";
 
 import useTitle from "../../hooks/useTitle";
 
 const title = "Tracker • Dashboard";
 
 const Dashboard = () => {
+  const { user } = useContext(AuthenticationContext);
+
+  const { displayName: name } = user;
+
   useTitle(title);
 
   return (
     <div>
       <h1>Dashboard</h1>
+
+      <p>Hello {name}.</p>
     </div>
   );
 };

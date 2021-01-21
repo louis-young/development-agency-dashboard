74f1c55ca2509ff664675179e96dbd0e4518114d
Louis Young <me@louisyoung.co.uk>
2021-01-21 20:44:22 +0000

    Style `Search` component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import "./Search.scss";
+
 const Search = ({ search, setSearch, placeholder }) => {
   return (
     <input
       name="search"
       type="text"
+      className="search"
       placeholder={placeholder}
       value={search}
       onChange={(event) => setSearch(event.target.value)}

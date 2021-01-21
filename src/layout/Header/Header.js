c122d6c4f71779fae313c8ce837edaca805ec568
Louis Young <me@louisyoung.co.uk>
2021-01-21 19:59:44 +0000

    Style `Header`

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -2,13 +2,17 @@ import React, { useContext } from "react";
 
 import { AuthenticationContext } from "../../context/AuthenticationContext";
 
+import "./Header.scss";
+
 const Header = () => {
   const { signOut } = useContext(AuthenticationContext);
 
   return (
-    <header>
-      <h2>Header</h2>
-      <button onClick={signOut}>Sign Out</button>
+    <header className="header">
+      <h3 className="header__logo">Tracker</h3>
+      <button className="header__button button button--light button--small" onClick={signOut}>
+        Sign Out
+      </button>
     </header>
   );
 };

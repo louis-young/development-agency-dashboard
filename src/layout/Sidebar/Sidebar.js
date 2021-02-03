18338c5038464597678e0a71acc870f85d966c72
Louis Young <me@louisyoung.co.uk>
2021-02-03 19:18:03 +0000

    Reorder `Sidebar`

--- src/layout/Sidebar/Sidebar.js
+++ src/layout/Sidebar/Sidebar.js
@@ -3,8 +3,8 @@ import React from "react";
 import { ROUTES } from "../../constants/constants";
 
 import dashboard from "../../assets/sidebar/dashboard.svg";
-import projects from "../../assets/sidebar/projects.svg";
 import clients from "../../assets/sidebar/clients.svg";
+import projects from "../../assets/sidebar/projects.svg";
 import domains from "../../assets/sidebar/domains.svg";
 import documentation from "../../assets/sidebar/documentation.svg";
 
@@ -18,8 +18,8 @@ const Sidebar = () => {
       <nav>
         <ul className="sidebar__list">
           <SidebarLink text="Dashboard" link={ROUTES.DASHBOARD} icon={dashboard} />
-          <SidebarLink text="Projects" link={ROUTES.PROJECTS} icon={projects} />
           <SidebarLink text="Clients" link={ROUTES.CLIENTS} icon={clients} />
+          <SidebarLink text="Projects" link={ROUTES.PROJECTS} icon={projects} />
           <SidebarLink text="Domains" link={ROUTES.DOMAINS} icon={domains} />
           <SidebarLink text="Documentation" link={ROUTES.DOCUMENTATION} icon={documentation} />
         </ul>
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -6,13 +6,13 @@ import { ROUTES } from "../constants/constants";
 
 import Dashboard from "../pages/Dashboard/Dashboard";
 
-import ProjectForm from "../components/Forms/ProjectForm/ProjectForm";
-import Project from "../pages/Projects/Project/Project";
-import Projects from "../pages/Projects/Projects";
-
 import ClientForm from "../components/Forms/ClientForm/ClientForm";
 import Client from "../pages/Clients/Client/Client";
 import Clients from "../pages/Clients/Clients";
+
+import ProjectForm from "../components/Forms/ProjectForm/ProjectForm";
+import Project from "../pages/Projects/Project/Project";
+import Projects from "../pages/Projects/Projects";
 
 import DomainForm from "../components/Forms/DomainForm/DomainForm";
 import Domains from "../pages/Domains/Domains";
@@ -32,15 +32,15 @@ const Routes = () => {
     <Switch>
       <Route path={ROUTES.DASHBOARD} exact component={Dashboard} />
 
-      <Route path={`${ROUTES.PROJECTS}${ACTIONS.ADD}`} component={ProjectForm} />
-      <Route path={`${ROUTES.PROJECTS}${ACTIONS.EDIT}`} component={ProjectForm} />
-      <Route path={`${ROUTES.PROJECTS}${ACTIONS.VIEW}`} component={Project} />
-      <Route path={ROUTES.PROJECTS} component={Projects} />
-
       <Route path={`${ROUTES.CLIENTS}${ACTIONS.ADD}`} component={ClientForm} />
       <Route path={`${ROUTES.CLIENTS}${ACTIONS.EDIT}`} component={ClientForm} />
       <Route path={`${ROUTES.CLIENTS}${ACTIONS.VIEW}`} component={Client} />
       <Route path={ROUTES.CLIENTS} component={Clients} />
+
+      <Route path={`${ROUTES.PROJECTS}${ACTIONS.ADD}`} component={ProjectForm} />
+      <Route path={`${ROUTES.PROJECTS}${ACTIONS.EDIT}`} component={ProjectForm} />
+      <Route path={`${ROUTES.PROJECTS}${ACTIONS.VIEW}`} component={Project} />
+      <Route path={ROUTES.PROJECTS} component={Projects} />
 
       <Route path={`${ROUTES.DOMAINS}${ACTIONS.ADD}`} component={DomainForm} />
       <Route path={`${ROUTES.DOMAINS}${ACTIONS.EDIT}`} component={DomainForm} />
